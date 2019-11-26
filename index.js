0b6d342b7c5da1d84cbd804d9026c817ce5fa46b
Sean Tayler <seantayler@gAM7acd39.local>
2019-11-26 10:53:43 -0700

    setup questions

---
+++ index.js
@@ -0,0 +1,180 @@
+Key = {
+    Padawin: "ESSENTIAL KNOWLEDGE - Q1 Basics",
+    Apprentice: "BENEFICIAL KNOWLEDGE - Q1 Intermediate",
+    Jedi: "I BOW TO YOU - Q1 Advanced"
+}
+
+
+/////////////////////////////////////////////////////////////////////////
+// Q2 - PADAWIN (MUST KNOW!!!)
+/////////////////////////////////////////////////////////////////////////
+
+//Q: Why use React? What are the benefits?
+//A: stores a value
+
+//Q: What is JSX? What are the benefits?
+//A: 
+
+//Q: How do you do conditional rendering in React?
+//A: 
+
+//Q: Exaplain the data flow in React?
+//A: 
+
+//Q: What do all components return?
+//A: 
+
+//Q: What causes a component to re-render?
+//A: 
+
+//Q: How do I loop over an array and create multiple instances of a component?
+//A: 
+
+//Q: What are the differences between state and functional components?
+//A: 
+
+//Q: What is state?
+//A: 
+
+//Q: How do you update the state of a component?
+//A: 
+
+//Q: What are the steps to update an input in React?
+//A: 
+
+//Q: What is props?
+//A: 
+
+//Q: How do you declare a prop and how do you access it in the child component?
+//A: 
+
+//Q: What are the two ways to get props in a component? (React + Redux)
+//A: 
+
+//Q: What are the 3 main lifecycle stages?
+//A: 
+
+//Q: Which lifecycle method should be used to make an API call and why?
+//A: 
+
+//Q: What is Redux? What are the benefits of Redux?
+//A: 
+
+//Q: Explain the data flow in Redux
+//A: 
+
+//Q: What is the store in Redux and what 2 things can we access from it?
+//A: 
+
+//Q: What is dispatch used for?
+//A: 
+
+//Q: Explain mapStateToProps
+//A: 
+
+//Q: Explain mapDispatchToProps
+//A: 
+
+//Q: What is an action creator?
+//A: 
+
+//Q: What is an action? What properties does it have?
+//A: 
+
+//Q: What is the role of a reducer?
+//A: 
+
+//Q: What do we need to do in a component to call an action creator and get it to the reducer?
+//A: 
+
+//Q: What is react router and why is it useful?
+//A: 
+
+//Q: What is a Link tag used for and what attribute does it use?
+//A: 
+
+//Q: What is a Route and what 2 attributes does it use?
+//A: 
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+// Q2 - APPRENTICE (GOOD TO KNOW!)
+/////////////////////////////////////////////////////////////////////////
+
+
+//Q: How do we bind a method to its component?
+//A: 
+
+
+//Q: What are the similarities between state and props?
+//A: 
+
+//Q: What are the differences between state and props?
+//A: 
+
+//Q: What are the arguments that setState takes? (1st is required/2nd is optional)
+//A: 
+
+//Q: What are propTypes used for?
+//A: 
+
+//Q: What do we mean by 'Component Lifecycle'?
+//A: 
+
+//Q: What is the difference between mounting and rendering?
+//A: 
+
+//Q: What are the main Lifecycle methods used (name at least 1) in each of the 3 lifecycle stages?
+//A: 
+
+//Q: What do we mean by 'single source of truth'?
+//A: 
+
+//Q: What is Redux Thunk? How does it work?
+//A: 
+
+//Q: What does the Provider do in React-Redux?
+//A: 
+
+//Q: What does Connect do in React-Redux?
+//A: 
+
+//Q: What two arguments do we pass connect?
+//A: 
+
+//Q: decribe how to write a reducer. What are its parameters and syntax?
+//A: 
+
+//Q: What is Switch tag used for in React-Router?
+//A: 
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+// Q2 - JEDI (ON YOUR WAY TO BE A MASTER)
+/////////////////////////////////////////////////////////////////////////
+
+//Q: What is the virtual DOM? How does it work?
+//A: 
+
+
+//Q: How can you force a component to rerender?
+//A: 
+
+//Q: What method is used to create a Store? What two arguments do we usually pass it?
+//A: 
+
+//Q: What props does BrowserRouter provide to its children?
+//A: 
+
+//Q: How can I access the value of a URL param?
+//A: 
+
